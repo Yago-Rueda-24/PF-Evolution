9fc3901b5ff3bb63069efc5867ba66340f80f3e8
yago-vasco <yago.rc.04@gmail.com>
2025-11-30 22:49:51 +0100

    Rutas y navegación entre componentes

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,6 +1,7 @@
 import { HashRouter, Routes, Route } from 'react-router-dom'
 import LoginForm from './components/LoginForm'
 import Vault from './components/Vault'
+import Generator from './components/Generator'
 
 function App(): React.JSX.Element {
   return (
@@ -8,6 +9,7 @@ function App(): React.JSX.Element {
       <Routes>
         <Route path="/" element={<LoginForm />} />
         <Route path="/vault" element={<Vault />} />
+        <Route path="/generator" element={<Generator />} />
       </Routes>
     </HashRouter>
   )
--- src/renderer/src/components/VaultSidebar.tsx
+++ src/renderer/src/components/VaultSidebar.tsx
@@ -1,12 +1,23 @@
 import React from 'react'
+import { Link, useLocation } from 'react-router-dom'
 
 const VaultSidebar = (): React.JSX.Element => {
+    const location = useLocation()
+
+    const isActive = (path: string) => {
+        return location.pathname === path ? { color: 'red', fontWeight: 'bold' } : { color: 'blue' }
+    }
+
     return (
         <aside className="vault-aside">
             <nav>
                 <ul>
-                    <li style={{ color: 'blue' }}>Dashboard</li>
-                    <li style={{ color: 'blue' }}>Settings</li>
+                    <li>
+                        <Link to="/vault" style={isActive('/vault')}>Vault</Link>
+                    </li>
+                    <li>
+                        <Link to="/generator" style={isActive('/generator')}>Generator</Link>
+                    </li>
                 </ul>
             </nav>
         </aside>
